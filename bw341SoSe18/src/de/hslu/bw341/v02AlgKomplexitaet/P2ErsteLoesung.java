f6128552636d165703a54b77e7961ae0a0931dec
Klaus Freyburger <klaus.freyburger@hs-lu.de>
2018-02-13 12:04:47 +0100

    Commt

---
+++ bw341SoSe18/src/de/hslu/bw341/v02AlgKomplexitaet/P2ErsteLoesung.java
@@ -0,0 +1,21 @@
+package de.hslu.bw341.v02AlgKomplexitaet;
+
+public class P2ErsteLoesung {
+	public static void main(String[] args) {
+		int[] folge = { 5, -8, 3, 3, -5, 7, -2, -7, 3, 5 };
+
+		// maximale Teilsumme ist mindestens 0 (Summe der leeren Teilfolge)
+		int maxTeilsumme = 0;
+
+		for (int anfang = 0; anfang < folge.length; anfang++) {
+			for (int ende = anfang; ende < folge.length; ende++) {
+				int Teilsumme = 0;
+				for (int element = anfang; element <= ende; element++)
+					Teilsumme = Teilsumme + folge[element];
+				if (Teilsumme > maxTeilsumme)
+					maxTeilsumme = Teilsumme;
+			}
+		}
+		System.out.println(maxTeilsumme);
+	}
+}
