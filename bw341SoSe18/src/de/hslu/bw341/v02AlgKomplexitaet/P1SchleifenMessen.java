1790714e23f7572d479e508f5e36d05057f7a90e
Klaus Freyburger <klaus.freyburger@hs-lu.de>
2018-04-12 10:36:09 +0200

    Schleifen messen

--- bw341SoSe18/src/de/hslu/bw341/v02AlgKomplexitaet/P1SchleifenMessen.java
+++ bw341SoSe18/src/de/hslu/bw341/v02AlgKomplexitaet/P1SchleifenMessen.java
@@ -13,7 +13,7 @@ public class P1SchleifenMessen {
 		long count = 0;
 
 		for (int i = 0; i < n; i++) 
- 			for (int j = i; j < n; j++) 
+// 			for (int j = 0; j < n; j++) 
 //     		for (int k = 0; k < n; k++) 
 					count++;
 
